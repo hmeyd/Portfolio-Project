7e8acd50a252082d42eef81b001229b343aae389
hmeyd <yyahmedsalm@example.com>
2025-07-07 14:00:11 +0200

    Initial commit - projet stage_4

---
+++ init_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+# Connexion ou création du fichier users.db
+conn = sqlite3.connect("users.db")
+c = conn.cursor()
+
+# Création de la table users
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        lastname TEXT NOT NULL,
+        email TEXT UNIQUE,
+        phone TEXT UNIQUE,
+        password TEXT NOT NULL
+    )
+''')
+
+conn.commit()
+conn.close()
