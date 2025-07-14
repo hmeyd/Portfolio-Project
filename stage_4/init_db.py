15f9a727a072366b9e23c1b3e713d72c4f4d83b9
Mame-Penda <mamependasadio1@gmail.com>
2025-07-14 21:19:24 +0200

    aff all the file of the project

--- stage_4/init_db.py
+++ stage_4/init_db.py
@@ -8,9 +8,19 @@ c = conn.cursor()
 c.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        lastname TEXT NOT NULL,
         email TEXT UNIQUE,
         phone TEXT UNIQUE,
         password TEXT NOT NULL
+    )
+''')
+
+# Création de la table notes
+c.execute('''
+    CREATE TABLE IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT NOT NULL
     )
 ''')
 
